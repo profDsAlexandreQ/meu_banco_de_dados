aa6a8ac86292beb5db61f591b15e6527ed3912f7
profDsAlexandreQ <alexandrealmeida01@prof.educacao.sp.gov.br>
2026-05-06 13:19:41 -0300

    Fix SQL syntax for table creation

--- banco.sql
+++ banco.sql
@@ -1,27 +1,21 @@
 CREATE DATABASE escola;
-
 USE escola;
-
 CREATE TABLE aluno(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-    idade INT NOT NULL    
-);
+    idade INT NOT NULL);
 CREATE TABLE curso(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    curso_name VARCHAR(50)
-);
+    curso_name VARCHAR(50));
 CREATE TABLE professor(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100)
-);
+    nome VARCHAR(100));
 CREATE TABLE professor_curso(
     id_professor INT,
     id_curso INT,
     PRIMARY KEY (id_professor, id_curso),
     FOREIGN KEY (id_professor) REFERENCES professor(id),
-    FOREIGN KEY (id_curso) REFERENCES curso(id)
-);
+    FOREIGN KEY (id_curso) REFERENCES curso(id));
 INSERT INTO aluno (nome, idade) VALUES ('João', 20);
 INSERT INTO aluno (nome, idade) VALUES ('Maria', 22);  
 INSERT INTO aluno (nome, idade) VALUES ('Pedro', 19);
